79d03c1f8a3338577b3d8fd5cb0e8832c51af801
Agus Altamirano <agusalta002@gmail.com>
2025-02-11 13:13:03 -0300

    update mejora al loader de carga

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen flex-col">
+    <div className="fixed bottom-32 right-4 bg-white shadow-lg rounded-lg p-4 flex items-center space-x-3 border border-gray-300">
       <div className="loader"></div>
-      <p className="text-center text-xl font-bold p-4 pr-3">CARGANDO...</p>
+      <p className="text-gray-700 text-sm font-semibold">CARGANDO...</p>
     </div>
   );
 };
